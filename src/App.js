e338ef3b4c98fd579e6fa3b8d726a12bf2e0f22c
ShivamGairola2577 <shivamgairola30@gmail.com>
2026-03-18 21:31:28 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -250,7 +250,7 @@ useEffect(() => {
   path="/"
   element={
     <>
-      {searchText.trim() === "" && (
+                                             {searchText.trim() !== "" && (
 
         <>
           {/* Main Section */}
